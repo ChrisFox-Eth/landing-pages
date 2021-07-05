c35c01a4c36b0d23866ff7519db9556af80115c7
Chris Fox <chris.b.fox1@gmail.com>
2021-07-05 19:05:47 -0400

    next up

---
+++ build/precache-manifest.f7500fbc3c54be6cf6076760bf817c0c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a931e69a6b6075cfdf327bab837c72ac",
+    "url": "./index.html"
+  },
+  {
+    "revision": "053aeee06af45afe4c88",
+    "url": "./static/css/2.637c884e.chunk.css"
+  },
+  {
+    "revision": "c806a7a0d3291544a859",
+    "url": "./static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "053aeee06af45afe4c88",
+    "url": "./static/js/2.c739b4b2.chunk.js"
+  },
+  {
+    "revision": "21b2dd38606ac28751db0d936591a095",
+    "url": "./static/js/2.c739b4b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c806a7a0d3291544a859",
+    "url": "./static/js/main.7607d4ee.chunk.js"
+  },
+  {
+    "revision": "2e97abbe475f456807e1",
+    "url": "./static/js/runtime-main.f4dd2c88.js"
+  },
+  {
+    "revision": "2eb1cfc514b300a822d2a07135d98a76",
+    "url": "./static/media/foxbox.2eb1cfc5.png"
+  }
+]);
